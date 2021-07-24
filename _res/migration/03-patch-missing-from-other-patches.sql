a74d70e429ed4521d367d66b7c43671ae1850fa9
worgflores <worgflores@gmail.com>
2021-07-24 11:43:11 +0800

    ctc: upgrade

--- _res/migration/03-patch-missing-from-other-patches.sql
+++ _res/migration/03-patch-missing-from-other-patches.sql
@@ -16,3 +16,32 @@ alter table itemaccount_tag add constraint pk_itemaccount_tag primary key (objid
 go 
 create index ix_acctid on itemaccount_tag (acctid)
 go 
+
+
+
+update raf set 
+	raf.controlid = d.controlid,
+	raf.receivedstartseries = d.receivedstartseries,
+	raf.receivedendseries = d.receivedendseries,
+	raf.qtyreceived = d.qtyreceived,
+	raf.beginstartseries = d.beginstartseries,
+	raf.beginendseries = d.beginendseries,
+	raf.qtybegin = d.qtybegin,
+	raf.issuedstartseries = d.issuedstartseries,
+	raf.issuedendseries = d.issuedendseries,
+	raf.qtyissued = d.qtyissued,
+	raf.endingstartseries = d.endingstartseries,
+	raf.endingendseries = d.endingendseries,
+	raf.qtyending = d.qtyending,
+	raf.qtycancelled = d.qtycancelled,
+	raf.remarks = d.remarks 
+from remittance_af raf 
+	inner join af_control_detail d on d.objid = raf.objid 
+where raf.controlid is null 
+go 
+
+
+delete from af_control_detail where reftype = 'REMITTANCE' and txntype = 'FORWARD' 
+GO 
+
+
